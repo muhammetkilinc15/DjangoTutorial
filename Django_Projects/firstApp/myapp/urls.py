6aa07b90f5455a8b996f823a2853b3c49df2ad68
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-14 03:21:00 +0300

    Created the products Urls

--- Django_Projects/firstApp/myapp/urls.py
+++ Django_Projects/firstApp/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = {
-    path('',views.index,name='index'),
-    path('index',views.index,name='index'),
-    path('details',views.details,name='details'),
-    path('list',views.list,name='list')
+    path('', views.index, name='index'),
+    path('index', views.index),
+    path('details', views.details, name='details'),
+    path('<int:category_id>', views.getProductsByCategoryId),
+    path('<str:category>', views.getProductsByCategory)
 }
