dba7f89fbe2b4adf4d3ad311a086a1839739f6e3
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-14 11:29:30 +0300

    Added dynamic routing

--- Django_Projects/firstApp/myapp/views.py
+++ Django_Projects/firstApp/myapp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import redirect
-
+from django.urls import reverse
 # Create your views here.
 
 
@@ -14,7 +14,13 @@ data = {
 
 # http://127.0.0.1:8000/products/
 def index(request):
-    return HttpResponse("index Page")
+    list_items = ""
+    category_list = list(data.keys())
+    for category in category_list:
+        redirect_Path = reverse("products_by_category", args=[category]) #dynamically creates url for us
+        list_items += f'<li><a href="{redirect_Path}">{category}</a></li>'
+    html = f'<ul>{list_items}</ul>'
+    return HttpResponse(html)
 
 # http://127.0.0.1:8000/products/details/
 def details(request):
@@ -24,14 +30,17 @@ def details(request):
 def getProductsByCategoryId(request, category_id):
     category_list = list(data.keys())
     if category_id > len(category_list) or category_id <=0:
-        return HttpResponseNotFound("Invalid Category selection :(")
-    redirect_text = category_list[category_id-1]
-    return HttpResponseRedirect("/products/" + redirect_text)
+        return HttpResponseNotFound("<h5>Invalid Category selection :(</h5>")
+    
+    category_Name = category_list[category_id-1]
+   
+    redirect_Path = reverse("products_by_category",args=[category_Name])
+    return redirect(redirect_Path)
 
 # http://127.0.0.1:8000/products/{phone,computer,electronic}
 def getProductsByCategory(request, category):
     try:
         category_text = data[category]        
-        return HttpResponse(category_text)
+        return HttpResponse(f'<h1>{category_text}</h1>')
     except:
-        return HttpResponseNotFound('Incorrect Category')
+        return HttpResponseNotFound('<h1>Incorrect Category</h1>')
