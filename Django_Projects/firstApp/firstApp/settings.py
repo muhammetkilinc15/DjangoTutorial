04447d7b0bba660126ebab9f085dad503af7cc5c
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-15 13:08:23 +0300

    Added Path for global static files

--- Django_Projects/firstApp/firstApp/settings.py
+++ Django_Projects/firstApp/firstApp/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Global static için
+STATICFILES_DIRS = [
+    BASE_DIR /"static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
