ac26fd4bce85ae4969b41e29332a100382c7c571
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-17 15:29:57 +0300

    Added Product model

--- Django_Projects/firstApp/myapp/models.py
+++ Django_Projects/firstApp/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Product Model
+class Product(models.Model):
+    Name = models.CharField(max_length=50)
+    Price = models.DecimalField(max_digits=8,decimal_places=2)
+    description = models.CharField(max_length=200)
+    ImageUrl = models.CharField(max_length=20)
+    
+    
+    
