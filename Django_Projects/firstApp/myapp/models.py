6440877767006de74b3d9dc7c50ab5481687c494
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-17 17:58:38 +0300

    Add new field for Product Model

--- Django_Projects/firstApp/myapp/models.py
+++ Django_Projects/firstApp/myapp/models.py
@@ -8,6 +8,11 @@ class Product(models.Model):
     Price = models.DecimalField(max_digits=8,decimal_places=2)
     description = models.CharField(max_length=200)
     ImageUrl = models.CharField(max_length=20)
+    isActive = models.BooleanField(default=False)
+    Category = models.CharField(max_length=50,null=True)
+    
+    def __str__(self):
+        return f"{self.Name} {self.Price} {self.description}"
     
     
     
