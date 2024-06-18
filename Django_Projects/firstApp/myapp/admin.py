c9badfbe52b76d94daa499c60b272b0820f6acc2
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-18 14:56:52 +0300

    Added CategoryAdmin

--- Django_Projects/firstApp/myapp/admin.py
+++ Django_Projects/firstApp/myapp/admin.py
@@ -1,5 +1,45 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product ,Category
 # Register your models here.
 
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    # Admin panelinde listede gösterilecek alanlar
+    list_display = ("Name",)
+    
+    # Admin panelinde listedeki öğeler için bağlantı verilecek alanlar
+    list_display_links = ("Name",)
+    
+    # Admin panelinde filtreleme yapılacak alanlar
+    list_filter = ("Name",)
+    
+    # Admin panelinde aranabilir alanlar
+    search_fields = ("Name",)
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    # Admin panelinde listede gösterilecek alanlar
+    list_display = ("Name", "Price","Category","isActive")
+    
+    # Slug alanını Name alanına göre otomatik doldurmak için prepopulated_fields
+    prepopulated_fields = {"slug": ("Name",)}
+    
+    # Admin panelinde listede linkli gösterilecek  alanlar
+    list_display_links = ("Name",)
+   
+    # Admin panelinde yalnızca okunabilir alanlar
+        #readonly_fields = ("slug",)
+    
+    
+    # Admin panelinde filtreleme yapılacak alanlar
+    list_filter = ("Name", "Category")
+    
+    # Admin panelinde düzenlenebilir alanlar
+    #list_editable = ("isActive",)
+    
+    # Admin panelinde aranabilir alanlar
+    search_fields = ("Name",)
+    
+    
+    
+admin.site.register(Product ,ProductAdmin)
