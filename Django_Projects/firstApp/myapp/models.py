6e9444c3472c3bcdd862ae64fd3c6a38b38cebb0
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-18 14:56:18 +0300

    Added Category Model

--- Django_Projects/firstApp/myapp/models.py
+++ Django_Projects/firstApp/myapp/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.utils.text import slugify # For save method
 # Create your models here.
 
+
+# Category Model
+class Category(models.Model):
+    Name = models.CharField(max_length=100)
+ 
+    # Özelliştirilmiş Verileri döndüren method
+    def __str__(self):
+        return f"{self.Name}"
+
+
 # Product Model
 class Product(models.Model):
     Name = models.CharField(max_length=50)
@@ -9,23 +19,14 @@ class Product(models.Model):
     description = models.CharField(max_length=200)
     ImageUrl = models.CharField(max_length=20)
     isActive = models.BooleanField(default=False)
-    Category = models.CharField(max_length=50,null=True)
-    slug = models.SlugField(default="",null=False,blank=True,editable=False,db_index=True,unique=True)
-
-    # slugify ile slug alanına Samsung S 22 degil de Samsung-S-22 yazilacak
-    def save(self, *args,**kargs):
-        self.slug= slugify(self.Name)
-        super().save(args,kargs)
+    Category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True,related_name="products")
+    slug = models.SlugField(default="",blank=True,null=False,db_index=True,unique=True)
 
     # Özelliştirilmiş Verileri döndüren method
     def __str__(self):
         return f"{self.Name} {self.Price} {self.description}"
-    
-    # def __str__(self):
-    #     # Tüm alanları almak için vars(self) kullanıyoruz
-    #     all_fields = vars(self)
-    #     # `Product` modelinin alanlarını key=value formatında bir string'e çeviriyoruz
-    #     fields_str = ', '.join(f'{key}={value}' for key, value in all_fields.items() if not key.startswith('_'))
-    #     return f'Product({fields_str})'
-
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.Name)
+        super().save(*args, **kwargs)
 
