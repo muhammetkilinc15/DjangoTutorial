d777ee9299d9b67f0920368c7ce304d95bd4f928
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-18 13:01:32 +0300

    Updated Product model

--- Django_Projects/firstApp/myapp/models.py
+++ Django_Projects/firstApp/myapp/models.py
@@ -10,15 +10,22 @@ class Product(models.Model):
     ImageUrl = models.CharField(max_length=20)
     isActive = models.BooleanField(default=False)
     Category = models.CharField(max_length=50,null=True)
-    slug = models.SlugField(default="",null=False,db_index=True,unique=True)
-    
-    # slugify ile slug alanına Samsung S 22 degil de Samsung-S-22 yazilacak 
+    slug = models.SlugField(default="",null=False,blank=True,editable=False,db_index=True,unique=True)
+
+    # slugify ile slug alanına Samsung S 22 degil de Samsung-S-22 yazilacak
     def save(self, *args,**kargs):
         self.slug= slugify(self.Name)
         super().save(args,kargs)
-    
+
+    # Özelliştirilmiş Verileri döndüren method
     def __str__(self):
         return f"{self.Name} {self.Price} {self.description}"
     
-    
-    
+    # def __str__(self):
+    #     # Tüm alanları almak için vars(self) kullanıyoruz
+    #     all_fields = vars(self)
+    #     # `Product` modelinin alanlarını key=value formatında bir string'e çeviriyoruz
+    #     fields_str = ', '.join(f'{key}={value}' for key, value in all_fields.items() if not key.startswith('_'))
+    #     return f'Product({fields_str})'
+
+
