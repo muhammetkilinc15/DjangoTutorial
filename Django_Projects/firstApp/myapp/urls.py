addf1f5ba8ac40f1b549440cca5a2bd4e243004b
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-18 10:35:01 +0300

    Add 'name' field to details URL

--- Django_Projects/firstApp/myapp/urls.py
+++ Django_Projects/firstApp/myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index),
-    path('details', views.details, name='details'),
+    path('<int:id>', views.details, name='details'),
     path('electronicPage', views.electronicPage, name='electronicPage'),
     path('<int:category_id>', views.getProductsByCategoryId),
     path('<str:category>', views.getProductsByCategory,name='products_by_category')
